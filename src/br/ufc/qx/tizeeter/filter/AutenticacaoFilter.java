06ccd789199f1a4db45fbf75e43dde330a38508f
Jordão Macedo <jordao05@hotmail.com>
2017-06-26 12:04:53 -0300

    Atualizações

--- src/br/ufc/qx/tizeeter/filter/AutenticacaoFilter.java
+++ src/br/ufc/qx/tizeeter/filter/AutenticacaoFilter.java
@@ -1,6 +1,7 @@
 package br.ufc.qx.tizeeter.filter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -24,21 +25,32 @@ public class AutenticacaoFilter implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		String path = ((HttpServletRequest)request).getRequestURI();
-		System.out.println(path);
-		if(path.equals("/Tizeeter_2/index.jsp")){
+		
+		ArrayList<String> urls= new ArrayList<String>();
+		urls.add("/Tizeeter_2/");
+		urls.add("/Tizeeter_2/index.jsp");
+		urls.add("/Tizeeter_2/todos");
+		
+		if(urls.contains(path)){
 			HttpSession sessao = ((HttpServletRequest)request).getSession(true);
 			if(sessao.getAttribute("usuario") != null && sessao.getAttribute("autenticado") != null && (boolean)sessao.getAttribute("autenticado")){
+				if(path.equals("/Tizeeter_2/todos")){
+					if(!((boolean)sessao.getAttribute("admin"))){
+						((HttpServletResponse)response).sendRedirect("/Tizeeter_2/403.jsp");
+						return;
+					}
+				}
 				chain.doFilter(request, response);
 				return;
 			}else{
 				((HttpServletResponse)response).sendRedirect("/Tizeeter_2/login.jsp");
+				return;
 			}
 		}
 		chain.doFilter(request, response);
 	}
 
 	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-	}
+	public void init(FilterConfig arg0) throws ServletException {}
 
 }
